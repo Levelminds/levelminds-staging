29c1feed2994647a5015e4d03fb2c9f6bbfaf6aa
Levelminds <dev@levelminds.in>
2025-10-14 20:49:03 +0530

    Modernize header and tour slider

--- partials/header.php
+++ partials/header.php
@@ -11,6 +11,9 @@ if (!isset($pageTitle)) { $pageTitle = "LevelMinds"; }
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&family=Public+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJ+Y3VCRa14O/2QRd8G8jM7Qp1aijB4Z2sqI=" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" integrity="sha384-9a6QJzi10BGSAdoo6gWQBaIj++ImQxGc1dQc5sKXc5teLoI0lp4rWuIwoMvV7lYd" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" integrity="sha384-Rq9BYwygJyI072QtdgQXl3k5ufADG7n2AFDzy83H8XTur2qxGn8pY/+bexdFv+Fr" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="assets/css/styles.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" integrity="sha512-pvOCc7n4/jr2dI+tvwlHGawEq+y3OC/YLXTr4a9L5xRcmk6VwBEm90LkAMPye/+qvOBvybOkHqvCO6L73X6zRg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="icon" type="image/svg+xml" href="assets/img/logo.svg" />
@@ -20,19 +23,15 @@ if (!isset($pageTitle)) { $pageTitle = "LevelMinds"; }
   STAGING SITE
 </div>
 <header class="site-header" data-header>
-  <div class="site-header__background" aria-hidden="true">
-    <span class="site-header__orb site-header__orb--primary"></span>
-    <span class="site-header__orb site-header__orb--secondary"></span>
-  </div>
-  <div class="container nav">
+  <div class="container site-header__inner">
     <a class="nav__brand" href="index.php" aria-label="LevelMinds home">
       <img class="nav__logo-img" src="assets/img/logo.svg" alt="LevelMinds logo">
       <span class="nav__title">LevelMinds</span>
     </a>
-    <button class="nav__toggle" type="button" aria-expanded="false" aria-controls="site-nav" data-nav-toggle>
+    <button class="nav__toggle" type="button" aria-expanded="false" aria-controls="site-nav" aria-label="Toggle navigation" data-nav-toggle>
       <span></span>
     </button>
-    <div class="nav__menu" id="site-nav" data-nav-menu>
+    <div class="nav__menu" id="site-nav" data-nav-menu aria-hidden="true">
       <nav class="nav__links" aria-label="Primary navigation">
         <a href="index.php">Home</a>
         <a href="team.php">Team</a>
@@ -41,7 +40,7 @@ if (!isset($pageTitle)) { $pageTitle = "LevelMinds"; }
         <a href="contact.php">Contact</a>
       </nav>
       <div class="nav__actions">
-        <a class="btn btn-primary" href="https://lmap.in/login">
+        <a class="btn btn-primary nav__cta" href="https://lmap.in/login">
           <svg class="icon icon--spark" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
             <path d="M12 3.25 13.62 8l4.88 1.12L14.5 12l.74 5-3.24-2.62L8.76 17l.74-5-3.99-2.88L10.5 8 12 3.25Z" fill="currentColor"/>
           </svg>
@@ -50,4 +49,5 @@ if (!isset($pageTitle)) { $pageTitle = "LevelMinds"; }
       </div>
     </div>
   </div>
+  <div class="nav__backdrop" data-nav-backdrop aria-hidden="true"></div>
 </header>
